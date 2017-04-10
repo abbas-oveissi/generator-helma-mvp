dfe7dfaf88fce03497c62a8ae707662dc0ab8c22
misaghlb <misaghlb@gmail.com>
2017-04-10 11:17:36 +0430

    create app directory and put everything in that

--- generators/app/index.js
+++ generators/app/index.js
@@ -71,50 +71,50 @@ module.exports = generator.extend({
   writing: function () {
     var packageDir = this.props.appPackage.replace(/\./g, '/');
 
-    this.config.set("appPackage",this.props.appPackage);
-    this.config.set("appName",this.props.appName);
+    this.config.set("appPackage", this.props.appPackage);
+    this.config.set("appName", this.props.appName);
 
-
-    mkdirp('app');
-    mkdirp('app/src/main/assets');
-    mkdirp('app/src/main/java/' + packageDir);
-    mkdirp('app/src/androidTest/java/' + packageDir);
-    mkdirp('app/src/test/java/' + packageDir);
+    mkdirp(this.props.appName); // create app directory that all files will create in that and will be root of project
+    mkdirp(this.props.appName + '/app');
+    mkdirp(this.props.appName + '/app/src/main/assets');
+    mkdirp(this.props.appName + '/app/src/main/java/' + packageDir);
+    mkdirp(this.props.appName + '/app/src/androidTest/java/' + packageDir);
+    mkdirp(this.props.appName + '/app/src/test/java/' + packageDir);
 
     // Copy files
-    this.fs.copy(this.templatePath('_gitignore'), this.destinationPath('.gitignore'));
-    this.fs.copy(this.templatePath('app/_gitignore'), this.destinationPath('app/.gitignore'));
-    this.fs.copy(this.templatePath('build.gradle'), this.destinationPath('build.gradle'));
-    this.fs.copy(this.templatePath('gradle.properties'), this.destinationPath('gradle.properties'));
-    this.fs.copy(this.templatePath('gradlew.bat'), this.destinationPath('gradlew.bat'));
-    this.fs.copy(this.templatePath('settings.gradle'), this.destinationPath('settings.gradle'));
+    this.fs.copy(this.templatePath('_gitignore'), this.destinationPath(this.props.appName + '/.gitignore'));
+    this.fs.copy(this.templatePath('app/_gitignore'), this.destinationPath(this.props.appName + '/app/.gitignore'));
+    this.fs.copy(this.templatePath('build.gradle'), this.destinationPath(this.props.appName + '/build.gradle'));
+    this.fs.copy(this.templatePath('gradle.properties'), this.destinationPath(this.props.appName + '/gradle.properties'));
+    this.fs.copy(this.templatePath('gradlew.bat'), this.destinationPath(this.props.appName + '/gradlew.bat'));
+    this.fs.copy(this.templatePath('settings.gradle'), this.destinationPath(this.props.appName + '/settings.gradle'));
     // Copy directories
-    this.fs.copy(this.templatePath('gradle'), this.destinationPath('gradle'));
-    this.fs.copy(this.templatePath('app/src/main/assets'), this.destinationPath('app/src/main/assets'));
-    this.fs.copy(this.templatePath('app/src/main/res'), this.destinationPath('app/src/main/res'));
+    this.fs.copy(this.templatePath('gradle'), this.destinationPath(this.props.appName + '/gradle'));
+    this.fs.copy(this.templatePath('app/src/main/assets'), this.destinationPath(this.props.appName + '/app/src/main/assets'));
+    this.fs.copy(this.templatePath('app/src/main/res'), this.destinationPath(this.props.appName + '/app/src/main/res'));
 
 
     // Copy template files
-    this.fs.copyTpl(this.templatePath('app/proguard-rules.pro'), this.destinationPath('app/proguard-rules.pro'), this.props);
-    this.fs.copyTpl(this.templatePath('libraries.gradle'), this.destinationPath('libraries.gradle'), this.props);
-    this.fs.copyTpl(this.templatePath('app/build.gradle'), this.destinationPath('app/build.gradle'), this.props);
-    this.fs.copyTpl(this.templatePath('app/src/main/AndroidManifest.xml'), this.destinationPath('app/src/main/AndroidManifest.xml'), this.props);
-    this.fs.copyTpl(this.templatePath('app/src/main/res/values/strings.xml'), this.destinationPath('app/src/main/res/values/strings.xml'), this.props);
+    this.fs.copyTpl(this.templatePath('app/proguard-rules.pro'), this.destinationPath(this.props.appName + '/app/proguard-rules.pro'), this.props);
+    this.fs.copyTpl(this.templatePath('libraries.gradle'), this.destinationPath(this.props.appName + '/libraries.gradle'), this.props);
+    this.fs.copyTpl(this.templatePath('app/build.gradle'), this.destinationPath(this.props.appName + '/app/build.gradle'), this.props);
+    this.fs.copyTpl(this.templatePath('app/src/main/AndroidManifest.xml'), this.destinationPath(this.props.appName + '/app/src/main/AndroidManifest.xml'), this.props);
+    this.fs.copyTpl(this.templatePath('app/src/main/res/values/strings.xml'), this.destinationPath(this.props.appName + '/app/src/main/res/values/strings.xml'), this.props);
 
     // Copy template directory
-    this.fs.copyTpl(this.templatePath('app/src/main/res/layout'), this.destinationPath('app/src/main/res/layout'), this.props);
-    this.fs.copyTpl(this.templatePath('app/src/androidTest/java/ir/harmas/temp'), this.destinationPath('app/src/androidTest/java/' + packageDir), this.props);
-    this.fs.copyTpl(this.templatePath('app/src/test/java/ir/harmas/temp'), this.destinationPath('app/src/test/java/' + packageDir), this.props);
-    this.fs.copyTpl(this.templatePath('app/src/main/java/ir/harmas/temp'), this.destinationPath('app/src/main/java/' + packageDir), this.props);
+    this.fs.copyTpl(this.templatePath('app/src/main/res/layout'), this.destinationPath(this.props.appName + '/app/src/main/res/layout'), this.props);
+    this.fs.copyTpl(this.templatePath('app/src/androidTest/java/ir/harmas/temp'), this.destinationPath(this.props.appName + '/app/src/androidTest/java/' + packageDir), this.props);
+    this.fs.copyTpl(this.templatePath('app/src/test/java/ir/harmas/temp'), this.destinationPath(this.props.appName + '/app/src/test/java/' + packageDir), this.props);
+    this.fs.copyTpl(this.templatePath('app/src/main/java/ir/harmas/temp'), this.destinationPath(this.props.appName + '/app/src/main/java/' + packageDir), this.props);
 
 
-    this.fs.delete(this.destinationPath('app/src/main/java/' + packageDir)+'/FloraShopApplication.java');
-    this.fs.copyTpl(this.templatePath('app/src/main/java/ir/harmas/temp/FloraShopApplication.java'), this.destinationPath(this.destinationPath('app/src/main/java/' + packageDir)+'/'+this.props.appName+'Application.java'), this.props);
+    this.fs.delete(this.destinationPath(this.props.appName + '/app/src/main/java/' + packageDir) + '/FloraShopApplication.java');
+    this.fs.copyTpl(this.templatePath('app/src/main/java/ir/harmas/temp/FloraShopApplication.java'), this.destinationPath(this.destinationPath(this.props.appName + '/app/src/main/java/' + packageDir) + '/' + this.props.appName + 'Application.java'), this.props);
 
+    this.fs.move(this.destinationPath(this.contextRoot + '/.yo-rc.json'), this.destinationPath(this.props.appName + '/.yo-rc.json')); // copy .yo-rc.json to project root directory
 
   }
 
 
 
 });
-
